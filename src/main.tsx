4c6e4ae9c8b0ae0bccbbe269e4893d87a8eb7dbb
Mykyta Zaluzhnyi <nikitaznr@gmail.com>
2024-10-07 20:45:57 +0200

    create theme + refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,44 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AppProvider } from "./contexts/AppContext";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AppProvider, useAppContext } from './contexts/AppContext';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import { ReactNode } from 'react';
+import { PaletteMode } from '@mui/material/styles';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const MyThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const { state } = useAppContext();
+  const settingValue = state.data.settings.find(
+    (setting) => setting.name === 'theme',
+  )?.value;
+
+  // Перевірка та приведення типу
+  const mode: PaletteMode =
+    settingValue === 'light' || settingValue === 'dark'
+      ? settingValue
+      : 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode,
+      primary: {
+        main: '#3f51b5',
+      },
+      secondary: {
+        main: '#f50057',
+      },
+    },
+  });
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
   <AppProvider>
-    <App />
-  </AppProvider>
+    <MyThemeProvider>
+      <App />
+    </MyThemeProvider>
+  </AppProvider>,
   // </React.StrictMode>
 );
